f198150adcb7f3d7cbf7dda036626807f3a98e9d
taco <taco@sly.nu>
2025-09-15 11:37:18 +1000

    add formatting of ExtraFS area used by MeshCore

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Adafruit_LittleFS.h>
 #include <InternalFileSystem.h>
 #include <Adafruit_TinyUSB.h>
+#include <CustomLFS.h>
 
 using namespace Adafruit_LittleFS_Namespace;
 
@@ -10,6 +11,10 @@ void setup() {
 
   delay(500);
   InternalFS.format();
+
+  delay(500);
+  CustomLFS ExtraFS(0xD4000, 0x19000, 128);
+  ExtraFS.format();
 
   delay(500);
   enterUf2Dfu();
